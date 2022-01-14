36953ea5c55946d17c3d7ebca58d42245caf6168
Thiago Henrique <fale@thiago.tech>
2022-01-14 09:29:21 -0300

    chore: change person struct

--- person.go
+++ person.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"reflect"
 
 	"github.com/bxcodec/faker/v3"
 )
@@ -9,14 +11,20 @@ import (
 type Person struct {
 	Name string `faker:"name" json:"name"`
 
-	Latitude  float32 `faker:"lat" json:"lat"`
-	Longitude float32 `faker:"long" json:"lng"`
+	Location Location `json:"location"`
 
-	DateTime string `faker:"time" json:"date_time"`
+	DateTime string `faker:"timestamp" json:"date_time"`
 	IDNumber string `faker:"len=15" json:"id_number"`
 }
 
+type Location struct {
+	Latitude  float32 `faker:"customLat" json:"lat"`
+	Longitude float32 `faker:"customLon" json:"lon"`
+}
+
 func GeneratePerson() (Person, error) {
+	addCustomGenerators()
+
 	person := Person{}
 
 	if err := faker.FakeData(&person); err != nil {
@@ -25,4 +33,20 @@ func GeneratePerson() (Person, error) {
 	}
 
 	return person, nil
+}
+
+func addCustomGenerators() {
+	var minLat float32 = 21.1
+	var maxLat float32 = 23.5
+
+	var minLon float32 = 46.7
+	var maxLon float32 = 49.2
+
+	_ = faker.AddProvider("customLat", func(v reflect.Value) (interface{}, error) {
+		return -(minLat + rand.Float32()*(maxLat-minLat)), nil
+	})
+
+	_ = faker.AddProvider("customLon", func(v reflect.Value) (interface{}, error) {
+		return -(minLon + rand.Float32()*(maxLon-minLon)), nil
+	})
 }
